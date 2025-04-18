9d01cd74dd19d111d3b817c5e123b466870ebf86
flyBoy667 <salifoudembele100@outlook.com>
2025-04-18 09:54:50 +0000

    Ajout de Flask-RESTful et définition d'un modèle de champ pour la température dans app.py.

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask_restful import Api, abort, Resource, marshal_with, fields
 
 app = Flask(__name__)
+api = Api(app)
 
+
+temp_fiels = {
+    "temperature": fields.String,
+}
 
 
 @app.route("/")
@@ -9,8 +15,5 @@ def index():
     return "Hello, World!"
 
 
-
-
-
 if __name__ == "__main__":
-    app.run(host='127.0.0.1',debug=True)
+    app.run(host="127.0.0.1", debug=True)
