552c576c81bfc70b85fc2a2774d8c319daba3e75
flyBoy667 <salifoudembele100@outlook.com>
2025-04-18 10:12:15 +0000

    Ajout d'une nouvelle ressource 'Temperatures' dans app.py avec des méthodes GET et POST, et mise à jour des champs de température pour inclure un timestamp.

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 from flask import Flask
-from flask_restful import Api, abort, Resource, marshal_with, fields
+from flask_restful import Api, abort, Resource, marshal_with, fields, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
 
-temp_fiels = {
-    "temperature": fields.String,
-}
+temp_fields = {"temperature": fields.String, "timestamp": fields.DateTime}
 
+temp_args = reqparse.RequestParser()
+temp_args.add_argument("temperature", type=float, required=True)
+temp_args.add_argument("timestamp", type=str, required=True)
+
+
+class Temperatures(Resource):
+    @marshal_with(temp_fields)
+    def get(self):
+        return {"temperature": 25}
+
+    def post(self):
+        args = temp_args.parse_args()
+        return {"message": "posting"}
+
+
+api.add_resource(Temperatures, '/temp')
 
 @app.route("/")
 def index():
